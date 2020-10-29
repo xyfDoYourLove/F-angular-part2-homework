445dc68688661920e1847ba9bfbe2d141de5480b
yifan.xiao <xyf15620168540@163.com>
2020-10-29 10:36:13 +0800

    [xiaoyifan]: add catchError for requirement 1

--- src/app/student.service.ts
+++ src/app/student.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Student } from './student';
 import { HttpClient } from '@angular/common/http';
-import {Observable} from 'rxjs';
+import {Observable, of} from 'rxjs';
+import {catchError} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -13,11 +14,22 @@ export class StudentService {
 
   // get url: /assets/students.mock.json
   getStudents(): Observable<Student[]> {
-    return this.http.get<Student[]>('/assets/students.mock.json');
+    return this.http.get<Student[]>('/assets/students.mock.json').pipe(
+      catchError(this.handleError<Student[]>('getStudents', []))
+    );
   }
 
   // get url: /assets/student{id}.mock.json
   getStudent(id: number): Observable<Student> {
-    return this.http.get<Student>(`/assets/student${id}.mock.json`);
+    return this.http.get<Student>(`/assets/student${id}.mock.json`).pipe(
+      catchError(this.handleError<Student>(`getStudent${id}`))
+    );
+  }
+
+  private handleError<T>(operation = 'operation', result?: T): (error: any) => Observable<T> {
+    return (error: any): Observable<T> => {
+      console.error(operation + ':' + error);
+      return of(result as T);
+    };
   }
 }
